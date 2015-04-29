e17fbfafb13bec3db7a59d671f9f7a19602473d2
weixiao <zhongwx@gmail.com>
2015-04-29 15:53:43 +0800

    daily

---
+++ Yue.Bookings.Contract/Actions/BookingActionPM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yue.Common.Contract;
+
+namespace Yue.Bookings.Contract.Actions
+{
+    public class BookingActionPM : BookingActionBase, IPersistenceModel
+    {
+        public DateTime From { get; private set; }
+        public DateTime To { get; private set; }
+        public int Minutes { get; private set; }
+
+        public void Persist()
+        {
+            From = TimeSlot.From;
+            To = TimeSlot.To;
+            Minutes = TimeSlot.Minutes;
+        }
+
+        public void Rehydrate()
+        {
+            this.TimeSlot = new Contract.TimeSlot(From, To);
+        }
+    }
+}
---
+++ Yue.Common.Contract/IPersistenceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yue.Common.Contract
+{
+    public interface IPersistenceModel
+    {
+        void Persist();
+        void Rehydrate();
+    }
+}
