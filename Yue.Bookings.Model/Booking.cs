5403c9931d413a394f74d65e2fa4fc38e7b70f18
weixiao <zhongwx@gmail.com>
2015-05-06 17:10:48 +0800

    IAggregateRoot

--- Yue.Bookings.Model/Booking.cs
+++ Yue.Bookings.Model/Booking.cs
@@ -11,7 +11,7 @@ using Yue.Common.Contract;
 
 namespace Yue.Bookings.Model
 {
-    public class Booking
+    public class Booking : IAggregateRoot
     {
         public int ResourceId { get; private set; }
         public int BookingId { get; private set; }
