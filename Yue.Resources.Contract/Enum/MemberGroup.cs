5e763aaeb24b62dc06d677e0d557a3b3d2d99e4c
weixiao <zhongwx@gmail.com>
2015-11-19 10:17:02 +0800

    Resources Contracts

---
+++ Yue.Resources.Contract/Enum/MemberGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yue.Resources.Contract
+{
+    public enum MemberGroup
+    {
+        Resource = 0,
+        Organization = 1
+    }
+}
---
+++ Yue.Resources.Contract/Enum/ResourcesState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yue.Resources.Contract
+{
+    public enum ResourcesState
+    {
+        Initial = 0,
+        Normal = 1,
+        Closed = 10,
+        Destroyed = 20,
+    }
+}
